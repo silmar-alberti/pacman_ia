6dca7171ce7856c6bb4158e59f9f338c42143520
silmar-alberti <alberti.silmar@gmail.com>
2016-09-04 11:26:48 -0300

    first commit

---
+++ Profundidade.py
@@ -0,0 +1,53 @@
+import util
+
+def busca_largura(problema ,profundidade,estado,caminho,visitados,fronteira):
+    if estado not in visitados:
+        visitados.add(estado)
+        if not fronteira.is_empty():
+            fronteira.remove(estado)
+
+    if problema.goalTest(estado) :
+        return 1
+    elif profundidade == 0:
+        fronteira.add(estado,caminho)
+        return 0
+    else:
+        corte = False
+        acoes = problema.getActions(estado)
+
+        for acao in acoes:
+            filho = problema.getResult(estado,acao)
+            if filho not  in visitados:
+                caminho_filho = caminho+[acao]
+                resultado = busca_largura(problema,profundidade-1,filho,caminho_filho,visitados,fronteira)
+                if resultado == 0:
+                    corte = True
+                elif resultado != None:
+                    caminho+=[acao]
+                    return 1
+
+
+        if corte:
+            return 0
+        else:
+            return None
+
+def profundidade(problem):
+
+    estado_incial = problem.getStartState()
+    caminho = []
+
+    fronteira = util.Fronteira()
+
+    fronteira.add(estado_incial,[])
+
+    limite = 1
+    visitados = set()
+    while True:
+        if not fronteira.is_empty():
+            estado, caminho = fronteira.pop()
+            resultado = busca_largura(problem,limite ,estado,caminho,visitados,fronteira)
+            if resultado == 1:
+                break
+
+    return caminho
---
+++ search.py
@@ -0,0 +1,204 @@
+# search.py
+# ---------
+# Licensing Information:  You are free to use or extend these projects for
+# educational purposes provided that (1) you do not distribute or publish
+# solutions, (2) you retain this notice, and (3) you provide clear
+# attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
+# 
+# Attribution Information: The Pacman AI projects were developed at UC Berkeley.
+# The core projects and autograders were primarily created by John DeNero
+# (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
+# Student side autograding was added by Brad Miller, Nick Hay, and
+# Pieter Abbeel (pabbeel@cs.berkeley.edu).
+
+
+# search.py
+# ---------
+# Licensing Information:  You are free to use or extend these projects for
+# educational purposes provided that (1) you do not distribute or publish
+# solutions, (2) you retain this notice, and (3) you provide clear
+# attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
+# 
+# Attribution Information: The Pacman AI projects were developed at UC Berkeley.
+# The core projects and autograders were primarily created by John DeNero
+# (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
+# Student side autograding was added by Brad Miller, Nick Hay, and
+# Pieter Abbeel (pabbeel@cs.berkeley.edu).
+
+
+"""
+In search.py, you will implement generic search algorithms which are called by
+Pacman agents (in searchAgents.py).
+"""
+
+import util
+import Profundidade
+import sys
+import copy
+
+class SearchProblem:
+    """
+    This class outlines the structure of a search problem, but doesn't implement
+    any of the methods (in object-oriented terminology: an abstract class).
+
+    You do not need to change anything in this class, ever.
+    """
+
+    def getStartState(self):
+        """
+        Returns the start state for the search problem.
+        """
+        util.raiseNotDefined()
+
+    def goalTest(self, state):
+        """
+          state: Search state
+
+        Returns True if and only if the state is a valid goal state.
+        """
+        util.raiseNotDefined()
+
+    def getActions(self, state):
+        """
+        Given a state, returns available actions.
+        Returns a list of actions
+        """        
+        # util.raiseNotDefined()
+
+    def getResult(self, state, action):
+        """
+        Given a state and an action, returns resulting state.
+        """
+        util.raiseNotDefined()
+
+    def getCost(self, state, action):
+        """
+        Given a state and an action, returns step cost, which is the incremental cost 
+        of moving to that successor.
+        """
+        util.raiseNotDefined()
+
+    def getCostOfActions(self, actions):
+        """
+         actions: A list of actions to take
+
+        This method returns the total cost of a particular sequence of actions.
+        The sequence must be composed of legal moves.
+        """
+        util.raiseNotDefined()
+
+
+def tinyMazeSearch(problem):
+    """
+    Returns a sequence of moves that solves tinyMaze.  For any other maze, the
+    sequence of moves will be incorrect, so only use this for tinyMaze.
+    """
+    from game import Directions
+    s = Directions.SOUTH
+    w = Directions.WEST
+    return  [s, s, w, s, w, w, s, w]
+
+def breadthFirstSearch(problem):
+    """
+    Search the shallowest nodes in the search tree first.
+
+    You are not required to implement this, but you may find it useful for Q5.
+    """
+    "*** YOUR CODE HERE ***"
+    return  Profundidade.profundidade(problem)
+
+def nullHeuristic(state, problem=None):
+    """
+    A heuristic function estimates the cost from the current state to the nearest
+    goal in the provided SearchProblem.  This heuristic is trivial.
+    """
+    return 0
+
+def busca_profundidade(problema ,profundidade,estado,caminho,visitados,fronteira):
+
+    if problema.goalTest(estado) :
+        return 1
+    elif profundidade == 0:
+        return 0
+    else:
+
+        corte = False
+        acoes = problema.getActions(estado)
+        # acoes = reversed(acoes)
+        for acao in acoes:
+            filho = problema.getResult(estado,acao)
+            custo = visitados.getValue(filho)
+            if  custo == None or custo >= fronteira:
+                visitados.add(filho,fronteira)
+                resultado = busca_profundidade(problema,profundidade-1,filho,caminho,visitados,fronteira+1)
+                if resultado == 0:
+                    corte = True
+                elif resultado != None:
+                    caminho.push(acao)
+                    return 1
+
+        if corte:
+            return 0
+        else:
+            return None
+
+#
+def BPLRecursive(node, problem, limit, solution, v):
+    if not v.has_key(node):
+        v[node] = limit
+    if problem.goalTest(node):
+        return True
+    elif limit == 0:
+        return 0
+    else:
+        cut = False
+        for action in problem.getActions(node):
+            child = problem.getResult(node, action)
+            if not v.has_key(child):
+                resultado = BPLRecursive(child, problem, limit - 1, solution, v)
+                if resultado == 0:
+                    cut = True
+                elif resultado != None:
+                    solution.push(action)
+                    return True
+        if cut:
+            return 0
+        else:
+            return None
+
+#
+
+def iterativeDeepeningSearch(problem):
+    """
+    Perform DFS with increasingly larger depth.
+
+    Begin with a depth of 1 and increment depth by 1 at every step.
+    """
+    resultado = 0
+    estado = problem.getStartState()
+    caminho = util.Queue()
+
+    limite = 1
+    # fronteira =[]
+    # fronteira= set()
+
+    visitados = util.Fronteira()
+    visitados.add(estado,0)
+    while True:
+        resultado = busca_profundidade(problem,limite ,estado,caminho,visitados,1)
+        if resultado == 1:
+            break
+        limite += 1
+
+    return caminho.list
+
+
+def aStarSearch(problem, heuristic=nullHeuristic):
+    """Search the node that has the lowest combined cost and heuristic first."""
+    "*** YOUR CODE HERE ***"
+    util.raiseNotDefined()
+
+# Abbreviations
+bfs = breadthFirstSearch
+astar = aStarSearch
+ids = iterativeDeepeningSearch
